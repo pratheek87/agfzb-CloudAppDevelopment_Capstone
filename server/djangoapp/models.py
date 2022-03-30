9db62fd1df77faa2a6ba7b324c97e6080df0703e
pratheek87 <pratheek87@gmail.com>
2022-03-30 18:21:01 +0000

    code

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -42,6 +42,45 @@ class CarModel(models.Model):
                "Year: " + str(self.year)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st,state, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+
+        self.state = state
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, car_make, car_model, car_year, dealership, id, name, purchase, purchase_date, review):
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.dealership = dealership
+        self.id = id
+        self.name = name     
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.review = review
+        
+    def __str__(self):
+        return "Review: " + self.review
